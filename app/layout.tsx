055d750b0b2a4244eb6eb2820775e0b5949644ad
Momen-G-Ar <cp.momen@gmail.com>
2025-04-14 18:27:03 +0300

    make the pages and the db

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -23,13 +25,14 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body
-                suppressHydrationWarning
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                {children}
-            </body>
-        </html>
+        <ClerkProvider>
+            <html lang="en">
+                <body
+                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
